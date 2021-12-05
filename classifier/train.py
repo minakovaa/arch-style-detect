78bc075554d92f22e1d7500dd1a84a5f12f66101
Alexander Minakov <minakov.alexander.a@yandex.ru>
2021-12-05 19:49:22 +0300

    Add template for refactoring train from .ipynb

---
+++ classifier/train.py
@@ -0,0 +1,26 @@
+def split_train_test_val():
+    pass
+
+
+def create_dataloaders():
+    pass
+
+
+def model_load():
+    pass
+
+
+def grid_search_model_params():
+    pass
+
+
+def train_model():
+    """ Train model with fixed params and save checkpoints
+        every `every_k_epoch_saved_ckpt` epochs
+    """
+    pass
+
+
+def test_model():
+    """ Test model on test set and return metrics"""
+    pass
